b0487c593d27c50c2449fe6aed81cdc609d2c181
Yusuf Öz <yusufoz230644@gmail.com>
2021-11-05 19:16:00 +0300

    ilk ödev yüklendi.

---
+++ consol_uygulama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace consol_uygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı Giriniz :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz :");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Hoşgeldiniz "+name+" "+surname+" !"); 
+        }
+    }
+}
